913cd33618571e93e499273d546dec26255e81d2
KenanRicky <kaburaricky@gmail.com>
2025-06-23 18:42:55 +0400

    Corrected the file

--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://<kaburaricky>:<ricky2025>@gallery.wc344.mongodb.net/RickyCluster?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<kaburaricky>:<ricky2025>@gallery.wc344.mongodb.net/RickyCluster-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<kaburaricky>:<ricky2025>@gallery.wc344.mongodb.net/RickyCluster-test?retryWrites=true&w=majority',
+    production: 'mongodb+srv://kaburaricky:ricky2025@gallery.wc344.mongodb.net/RickyCluster?retryWrites=true&w=majority',
+    development: 'mongodb+srv://kaburaricky:ricky2025@gallery.wc344.mongodb.net/RickyCluster-dev?retryWrites=true&w=majority',
+    test: 'mongodb+srv://kaburaricky:ricky2025@gallery.wc344.mongodb.net/RickyCluster-test?retryWrites=true&w=majority',
 }
 module.exports = config;

c138a21a834e81fd8de85d74ff291160c9c03151
KenanRicky <kaburaricky@gmail.com>
2025-06-23 18:06:41 +0400

    Added some files

--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: 'mongodb+srv://<kaburaricky>:<ricky2025>@gallery.wc344.mongodb.net/RickyCluster?retryWrites=true&w=majority',
+    development: 'mongodb+srv://<kaburaricky>:<ricky2025>@gallery.wc344.mongodb.net/RickyCluster-dev?retryWrites=true&w=majority',
+    test: 'mongodb+srv://<kaburaricky>:<ricky2025>@gallery.wc344.mongodb.net/RickyCluster-test?retryWrites=true&w=majority',
 }
 module.exports = config;

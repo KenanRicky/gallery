6d6f25e5a5e8e5066de9df734c426ef79dff2384
KenanRicky <kaburaricky@gmail.com>
2025-06-23 20:57:07 +0400

    _config.js

--- _config.js
+++ _config.js
@@ -6,10 +6,4 @@ config.mongoURI = {
     development: 'mongodb+srv://kaburaricky:ricky2025@gallery.wc344.mongodb.net/RickyCluster-dev?retryWrites=true&w=majority',
     test: 'mongodb+srv://kaburaricky:ricky2025@gallery.wc344.mongodb.net/RickyCluster-test?retryWrites=true&w=majority',
 }
-module.exports = {
-  mongoURI: process.env.NODE_ENV === 'test' 
-    ? "mongodb://localhost:27017/gallery-test"
-    : "mongodb+srv://kaburaricky:ricky2025@gallery.wc344.mongodb.net/RickyCluster?retryWrites=true&w=majority"
-};
-
-
+module.exports = config;

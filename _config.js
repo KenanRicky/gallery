fae87363472458e8c6bd7056ce9f2ef28db6076a
KenanRicky <kaburaricky@gmail.com>
2025-06-24 22:33:48 +0400

    Corrected the files

---
+++ _config.js
@@ -0,0 +1,9 @@
+var config = {}
+
+// Update to have your correct username and password
+config.mongoURI = {
+    production: 'mongodb+srv://kaburaricky:ricky2025@rickycluster.yknl9gf.mongodb.net/?retryWrites=true&w=majority&appName=RickyCluster',
+    development: 'mongodb+srv://kaburaricky:ricky2025@rickycluster.yknl9gf.mongodb.net/?retryWrites=true&w=majority&appName=RickyCluster',
+    test: 'mongodb+srv://kaburaricky:ricky2025@rickycluster.yknl9gf.mongodb.net/?retryWrites=true&w=majority&appName=RickyCluster',
+}
+module.exports = config;

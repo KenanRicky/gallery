73ea36c46bdb3fd0d9a648499217b76a438c6c9b
KenanRicky <kaburaricky@gmail.com>
2025-06-24 21:54:30 +0400

    Fille added

--- _config.js
+++
@@ -1,9 +0,0 @@
-var config = {}
-
-// Update to have your correct username and password
-config.mongoURI = {
-    production: 'mongodb+srv://kaburaricky:ricky2025@gallery.wc344.mongodb.net/RickyCluster?retryWrites=true&w=majority',
-    development: 'mongodb+srv://kaburaricky:ricky2025@gallery.wc344.mongodb.net/RickyCluster-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://kaburaricky:ricky2025@gallery.wc344.mongodb.net/RickyCluster-test?retryWrites=true&w=majority',
-}
-
